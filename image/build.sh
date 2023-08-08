468e4028d98fb386df49adfb38cef84573b952ef
Gerred Dillon <hello@gerred.org>
2023-08-08 08:11:39 -0500

    Updates for WSL2 to work

--- image/build.sh
+++ image/build.sh
@@ -2,13 +2,13 @@
 
 set -euxo pipefail
 
-K3S_TAG=${K3S_TAG:="v1.27.2-k3s1"} # replace + with -, if needed
+K3S_TAG=${K3S_TAG:="v1.27.3-k3s1"} # replace + with -, if needed
 IMAGE_REGISTRY=${IMAGE_REGISTRY:="ghcr.io/runyontr/packages"}
 IMAGE_REPOSITORY=${IMAGE_REPOSITORY:="rancher/k3s"}
 IMAGE_TAG="$K3S_TAG-cuda"
 IMAGE=${IMAGE:="$IMAGE_REGISTRY/$IMAGE_REPOSITORY:$IMAGE_TAG"}
 
-NVIDIA_CONTAINER_RUNTIME_VERSION=${NVIDIA_CONTAINER_RUNTIME_VERSION:="3.5.0-1"}
+NVIDIA_CONTAINER_RUNTIME_VERSION=${NVIDIA_CONTAINER_RUNTIME_VERSION:="3.13.0"}
 
 echo "IMAGE=$IMAGE"
 
@@ -17,5 +17,5 @@ DOCKER_BUILDKIT=0 docker build \
   --build-arg K3S_TAG=$K3S_TAG \
   --build-arg NVIDIA_CONTAINER_RUNTIME_VERSION=$NVIDIA_CONTAINER_RUNTIME_VERSION \
   -t $IMAGE .
-docker push $IMAGE
+# docker push $IMAGE
 echo "Done!"
